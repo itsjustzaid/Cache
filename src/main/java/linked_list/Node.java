a2cb699ba15cbd662068325521398e038b6fc4d5
JustZaid <zaidkhanxdxd@gmail.com>
2024-01-20 23:52:22 +0530

    Add a type for node

---
+++ src/main/java/linked_list/Node.java
@@ -0,0 +1,12 @@
+package linked_list;
+
+public class Node<T> {
+    T data;
+    Node<T> next, prev;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
